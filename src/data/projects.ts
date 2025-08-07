faf315e076c9d31b10b09e532f0a2b5d11550f60
saurabh chauhan <sourabh1202@gmail.com>
2025-08-07 23:26:51 +0300

    updated project url

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,47 +5,47 @@ export const projects: IProjects[] = [{
     name: "Carpass",
     description: "An phone app used to make car inspection easy and efficient. It allows users to take photos of their car, add details, and share the inspection report with others.",
     url: "https://github.com/sirbh/carpass-lite",
-    youTubeUrl: "https://youtu.be/tZbWhJO7sBc"
+    youTubeUrl: "https://youtu.be/nFJws6NSSjY"
 },
 {
     id: "2",
     name: "Helsinki City Bike",
     description: "An app that provides information about Helsinki City Bike stations, including availability and location. It allows users to find the nearest bike station and check the number of bikes available.",
     url: "https://github.com/sirbh/helsinki-city-bike",
-    youTubeUrl: "https://youtu.be/6o1OvFcxOCw"
+    youTubeUrl: "https://youtu.be/LBahXSt3WGY"
 },
 {
     id: "3",
     name: "Tuni Rooms",
     description: "An App to search rooms for Tampere Unviersity students. It allows users to search and locate rooms accroos all the campuses of Tampere University.",
     url: "https://github.com/sirbh/tuni-rooms",
-    youTubeUrl: "https://youtu.be/Qp8-yGHwk7A"
+    youTubeUrl: "https://youtu.be/dfUTE8R17PU"
 }, 
 {
     id: "4",
     name: "Weather App Java",
     description: "An app that provides weather information for a given location. It allows users to search for weather conditions, view forecasts, and get current weather updates.",
     url: "https://github.com/sirbh/java-weather-app",
-    youTubeUrl: "https://youtu.be/dyjBxZ2m3Fw"
+    youTubeUrl: "https://youtu.be/BBl_6ntY9GY"
 },
 {
     id: "5",
     name: "Media Finder Pro",
     description: "An to search and filter through media items like books and movies. It allows users to search for media items, view details, and filter results based on various criteria.",
     url: "https://github.com/sirbh/weather-app-java",
-    youTubeUrl: "https://youtu.be/IFXYMEYKIdU"
+    youTubeUrl: "https://youtu.be/T5gHx18FWqM"
 },
 {
     id: "6",
     name: "Gen AI Book Search",
     description: "An chat application that allows users to search for books using natural language queries. It uses a generative AI model to understand user queries and provide relevant book recommendations.",
     url: "https://github.com/sirbh/genUI-bookstore",
-    youTubeUrl: "https://youtu.be/x8Iigdp94A8"
+    youTubeUrl: "https://youtu.be/Uoz1ZOcpZBM"
 },
 {
     id: "7",
     name: "Vibe Coder",
     description: "An app where user can create project by just providing natural language description. It uses generative AI to create a project structure and code based on the user's input. Backed by NextJs, LangChain, and OpenAI. It create empty NextJs project and updates it according to user description.",
     url: "https://github.com/sirbh/vibe-coder",
-    youTubeUrl: "https://youtu.be/ekxf3vUaCzo"
+    youTubeUrl: "https://youtu.be/olbD_0ALEa8"
 }] 
